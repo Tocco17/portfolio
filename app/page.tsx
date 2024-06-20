5d6db8ecff5f29757a3d9be839f0eeeb4853399d
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-20 15:15:56 +0200

    page container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { PageContainer } from "./components/page-container";
 import { PageTitle } from "./components/page-title";
 
 export default function Home() {
 	return (<>
 		<PageTitle title="Federico's Portfolio" />
 
-		<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-		</div>
+		<PageContainer>
+			<p></p>
+		</PageContainer>
 	</>);
 }
