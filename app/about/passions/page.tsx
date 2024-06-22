af8db013776b81238ff68d21b821773482256708
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-22 12:42:48 +0200

    Pagina passioni

--- app/about/passions/page.tsx
+++ app/about/passions/page.tsx
@@ -1,5 +1,36 @@
 export const PassionsPage = () => {
 	return (<>
 		<div>Passions page</div>
+		<p>
+			Sono un ragazzo dalle mille passioni, 
+			che segue la regola "Finchè non hai provato non puoi dire che è cattivo".
+			Secondo questa filosofia voglio e devo ancora dare una chance alla pizza con l'ananas,
+			nonostante il mio sangue italiano mi obbliga ad esserne disgustato.
+
+			Faccio Karatè dal 2010. 
+			Sono cresciuto nella società di cui faccio parte tutt'oggi: Bushido Karatè ASD. 
+			Per un periodo ho insegnato questa disciplina a ragazzi, di età variabile da 5/6 anni in su.
+			Faccio parte del consiglio, occupandomi quindi di varie mansioni:
+			- organizzazione generale della società (legislativa e burocratica)
+			- gestione iscrizione
+			- organizzazione eventi sportivi
+			- responsabile delle politiche di safeguarding
+			- certificato BDSM: intervento in caso di ProntoSoccorso
+
+			Suono la batteria dal 2021
+			e sono appassionato di musica da ancora prima che fossi nei pensieri dei miei genitori.
+			Sempre in cerca di un nuovo concerto da vedere!
+
+			Nella classica domanda "mare o montagna?", scelgo il posto col paesaggio più bello!
+			Che poi questo generalmente coincida con la montagna è un caso...
+			Anche se sono appassionato di hiking e trekking, 
+			al momento ho solo la "Via degli Dei" nella lista dei "percorsi impegnativi completati".
+			Ma ho in programma molti altri percorsi da completare!
+			In generale comunque mi piace viaggiare, in compagnia o anche in solitaria.
+			Adoro perdermi nelle vie di città che esploro per la prima volta. 
+			Sono spesso finito in posti estremamente particolari così!
+
+			Ho la patente B dal 2016, e ho la patente A dal 2023.
+		</p>
 	</>)
 }
