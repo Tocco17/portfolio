4c8419b7368cbd0e6c521c1ba9c9500b367cd1d1
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-23 12:41:36 +0200

    work page

--- app/about/work/page.tsx
+++ app/about/work/page.tsx
@@ -1,5 +1,57 @@
 export const WorkPage = () => {
 	return (<>
 		<div>Work page</div>
+
+		<p>
+			Bushido Karate ASD 2018-2020
+			Istruttore di Karatè
+			Mansioni:
+			- Insegnante di persone in età infantile, preadolescenziale, adolelescenziale e adulta
+			- Organizzatore di eventi sportivi
+			- Gestione della clientela
+			- Pronto intervento (Corso di BLSD)
+
+			
+			Eustema 2021-2022
+			Fullstack Developer
+			Progetti:
+			- SIL
+				Gestionale di offerte di lavoro per la regione Sardegna
+				Stack:
+					- .NET 5
+					- .NET Framework 4.8
+					- SQL Server
+					- Dapper
+					- Razor 
+					- Bootstrap
+			- Corte dei Conti 
+				Plugin Microsoft Word e Outlook 
+				Stack:
+					- .NET WindowsForm
+			- SIAE 
+				Sanificazione dati
+
+			Remira Italia 2023-presente
+			Fullstack Developer
+			Progetti:
+			- MDSourcing 
+				Sito E-Commerce di vendita piante e alberi
+				Stack:
+					- Angular 
+					- .NET 5
+					- Entity Framework 
+					- Bootstrap
+			- Salary Calculette 
+				Gestionale contratti per l'EUI (European University Institute)
+				Stack:
+					- React ts
+					- .NET 7
+					- Entity Framework
+					- PostreSQL
+					- Docker 
+					- Tailwind CSS
+			
+			
+		</p>
 	</>)
 }
