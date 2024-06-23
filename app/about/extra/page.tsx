be7fb9685ba037255b719e513adacfeba6797334
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-23 15:45:11 +0200

    extra page

--- app/about/extra/page.tsx
+++ app/about/extra/page.tsx
@@ -1,5 +1,15 @@
 export const ExtraPage = () => {
 	return (<>
 		<div>Extra curriculum experience</div>
+		<p>
+			Patente A e B 
+
+			Batteria
+
+			Consigliere in Bushido Karatè
+
+			Teatro 
+			
+		</p>
 	</>)
 }
