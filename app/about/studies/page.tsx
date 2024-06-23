a0dd7bbebb5ada9293021e7b06dde6c866140715
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-23 10:13:57 +0200

    studies page

--- app/about/studies/page.tsx
+++ app/about/studies/page.tsx
@@ -1,5 +1,30 @@
 export const StudiesPage = () => {
 	return (<>
 		<div>Studies page</div>
+
+		<p>
+			Mi sono iscritto all'Università degli Studi di Firenze, scuola Fisica e Astrofisica.
+			
+			Il primo approccio all'informatica l'ho avuto proprio all'Università.
+			Fra i corsi obbligatori del primo c'era proprio il corso di informatica,
+			in cui hanno insegnato C.
+			La materia mi è da subito piaciuta, tant'è che ho iniziato quasi immediatamente
+			a studiarla anche per conto mio.
+			Sotto consiglio di amici sviluppatori, ho subito iniziato a studiare C#. 
+			
+			Mi sono laureato nel 2021, discutendo una tesi con tema Recommended System.
+			Breve descrizione dei Recommended System:
+			Sono dei modelli che puntano ad analizzare una popolazione,
+			con lo scopo di poter prevedere i loro comportamenti.
+			Sono sistemi oggi visibili dappertutto, soprattutto utilizzato nei siti di e-commerce,
+			proprio per la loro natura.
+			Come dice il nome, puntano a consigliare prodotti specifici alle persone,
+			analizzando diversi fattori, come il periodo dell'anno, la loro età, il loro genere,
+			la cultura della popolazione intorno, il loro comportamento all'interno dello stesso
+			sito di e-commerce.
+			I vari modelli studiati li ho scritti usando Matlab.
+
+			Successivamente alla laurea ho trovato quasi subito lavoro come programmatore.			
+		</p>
 	</>)
 }
