463034dbb98d43e384de776c0d6adeffef4f9e04
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-25 22:07:02 +0200

    skills page

--- app/about/skills/page.tsx
+++ app/about/skills/page.tsx
@@ -1,5 +1,44 @@
 export const SkillsPage = () => {
 	return (<>
 		<div>Skills page</div>
+		<p>
+			LINGUAGGI
+			javascript
+			jQuery 
+			typescript
+			C
+			C#
+			python 
+			php 
+			matlab 
+			html 5
+			css3 
+			SQL 
+		</p>
+		<p>
+			FRAMEWORK E LIBRERIE
+			react 
+			angular 
+			vue
+			laravel 
+			next js
+			.net 
+			material ui 
+			tailwind css 
+			Bootstrap
+			Entity Framework 
+			Dapper 
+		</p>
+		<p>
+			SOFTWARE
+			docker 
+			azure
+			aws 
+			xampp 
+			figma 
+			git 
+			node js 
+			PostgreSQL 
+		</p>
 	</>)
 }
