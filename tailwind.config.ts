95267e50701da7dd56fdab4ad2c20b16a5832856
FedericoRuocco <fede.ruocco@gmail.com>
2024-06-25 22:21:02 +0200

    tailwind config ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
 import type { Config } from "tailwindcss";
 
+const colors = require('tailwindcss/colors');
+
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				// Light colors
+				'primary-light': '#F7F8FC',
+				'secondary-light': '#FFFFFF',
+				'ternary-light': '#f6f7f8',
+
+				// Dark colors
+				'primary-dark': '#0D2438',
+				'secondary-dark': '#102D44',
+				'ternary-dark': '#1E3851',
+
+				// Extended v3 color
+				gray: colors.neutral,
+			},
+			container: {
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '5rem',
+					xl: '6rem',
+					'2xl': '8rem',
+				},
+			},
+		},
+	},
+	plugins: [],
 };
+
 export default config;
