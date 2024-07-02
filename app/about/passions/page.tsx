b4b50a0fb195b032acff367ad71b68210e524f78
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-02 17:06:01 +0200

    passions page

--- app/about/passions/page.tsx
+++ app/about/passions/page.tsx
@@ -2,38 +2,50 @@ import { NextPage } from "next"
 
 const PassionsPage: NextPage = () => {
 	return (<>
-		<div>Passions page</div>
-		<p>
-			Sono un ragazzo dalle mille passioni, 
-			che segue la regola "Finchè non hai provato non puoi dire che è cattivo".
-			Secondo questa filosofia voglio e devo ancora dare una chance alla pizza con l'ananas,
-			nonostante il mio sangue italiano mi obbliga ad esserne disgustato.
+		<h2>Passions page</h2>
 
-			Faccio Karatè dal 2010. 
-			Sono cresciuto nella società di cui faccio parte tutt'oggi: Bushido Karatè ASD. 
-			Per un periodo ho insegnato questa disciplina a ragazzi, di età variabile da 5/6 anni in su.
-			Faccio parte del consiglio, occupandomi quindi di varie mansioni:
-			- organizzazione generale della società (legislativa e burocratica)
-			- gestione iscrizione
-			- organizzazione eventi sportivi
-			- responsabile delle politiche di safeguarding
-			- certificato BDSM: intervento in caso di ProntoSoccorso
+		<div>
+			<p>
+				I am a person with many passions, following the rule "You can't say it's bad until you've tried it."
+				According to this philosophy, I still want and need to give pineapple pizza a chance,
+				despite my Italian blood obliging me to be disgusted by it.
+			</p>
 
-			Suono la batteria dal 2021
-			e sono appassionato di musica da ancora prima che fossi nei pensieri dei miei genitori.
-			Sempre in cerca di un nuovo concerto da vedere!
+			<p>
+				I have been practicing Karate since 2010.
+				I grew up in the society I am still part of today: Bushido Karate ASD.
+				For a period, I taught this discipline to children aged 5/6 and up.
+				I am part of the council, taking on various responsibilities, including:
+				<ul>
+					<li>General organization of the society (legislative and bureaucratic)</li>
+					<li>Enrollment management</li>
+					<li>Organization of sporting events</li>
+					<li>Safeguarding policies manager</li>
+					<li>Certified BDSM: first aid intervention</li>
+				</ul>
+			</p>
 
-			Nella classica domanda "mare o montagna?", scelgo il posto col paesaggio più bello!
-			Che poi questo generalmente coincida con la montagna è un caso...
-			Anche se sono appassionato di hiking e trekking, 
-			al momento ho solo la "Via degli Dei" nella lista dei "percorsi impegnativi completati".
-			Ma ho in programma molti altri percorsi da completare!
-			In generale comunque mi piace viaggiare, in compagnia o anche in solitaria.
-			Adoro perdermi nelle vie di città che esploro per la prima volta. 
-			Sono spesso finito in posti estremamente particolari così!
+			<p>
+				I have been playing the drums since 2021
+				and have been passionate about music even before I was in my parents' thoughts.
+				Always looking for a new concert to attend!
+			</p>
 
-			Ho la patente B dal 2016, e ho la patente A dal 2023.
-		</p>
+			<p>
+				When it comes to the classic "sea or mountains" question, I choose the place with the most beautiful scenery!
+				That it usually coincides with the mountains is just a coincidence...
+				Although I am passionate about hiking and trekking,
+				I currently only have the "Via degli Dei" on the list of "challenging routes completed."
+				But I have many other routes planned!
+				In general, I enjoy traveling, whether in company or solo.
+				I love getting lost in the streets of cities I am exploring for the first time.
+				I have often ended up in extremely peculiar places this way!
+			</p>
+
+			<p>
+				I have had a B driver's license since 2016 and an A license since 2023.
+			</p>
+		</div>
 	</>)
 }
 
