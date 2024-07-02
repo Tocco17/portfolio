d804d067153d3cf26797f1617c55e498ddfcc8ae
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-02 17:05:53 +0200

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,27 @@
 export default function AboutPage() {
 	return (<>
-		<div>About page</div>
-		<div>
-			<p>Panoramica di chi sono</p>
-			<p>
-				My name is Federico. I was born in Bagno a Ripoli in 1996. 
-				Experts in anthropology confirm that it was a great year.
+		<h2>About page</h2>
 
+		<div>
+			<p>
+				My name is Federico. I live in Florence but I was born in Bagno a Ripoli in 1996. 
+				Experts in anthropology confirm that it was a great year!
+			</p>
+
+			<p>
 				I am a physicist working as a developer. 
 				You could say that I chose a job where I am constantly 
 				surrounded by the number one enemy of a physicist: engineers!
+			</p>
 
-				I live in Florence.
-
+			<p>
 				I have always pursued scientific studies: 
 				I attended a science-oriented high school 
 				and then enrolled at the University of Florence, 
 				in the School of Physics and Astrophysics.
+			</p>
 
+			<p>
 				After graduating, I found a job as a programmer, 
 				a job I still do today.
 			</p>
