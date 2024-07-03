fc9ce291c7724b2eab9e171d5d2520fb9488e87c
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-03 22:34:55 +0200

    Layout

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -8,15 +8,17 @@ export default function AboutPageLayout({
 
 	return (<>
 		<div className="page-with-menu">
-			<ul className="horizontal-navbar">
-				<li><Link href="/about">Introduction</Link></li>
-				<li><Link href="/about/passions">Passions</Link></li>
-				<li><Link href="/about/studies">Studies</Link></li>
-				<li><Link href="/about/work">Work Experience</Link></li>
-				{/* <li><Link href="/about/extra">Extra curriculum experience</Link></li> */}
-				<li><Link href="/about/skills">Skills</Link></li>
-				{/* <li><Link href="/about/certificates">Certificates</Link></li> */}
-			</ul>
+			<div className="horizontal-navbar">
+				<ul>
+					<li><Link href="/about">Introduction</Link></li>
+					<li><Link href="/about/passions">Passions</Link></li>
+					<li><Link href="/about/studies">Studies</Link></li>
+					<li><Link href="/about/work">Work Experience</Link></li>
+					{/* <li><Link href="/about/extra">Extra curriculum experience</Link></li> */}
+					<li><Link href="/about/skills">Skills</Link></li>
+					{/* <li><Link href="/about/certificates">Certificates</Link></li> */}
+				</ul>
+			</div>
 			<div className="page">{children}</div>
 		</div>
 	</>)
