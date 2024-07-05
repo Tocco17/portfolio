bb25e38864e19f641b5f8940522d1bb995a464d2
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-05 08:36:21 +0200

    about layout

--- app/about/passions/page.tsx
+++ app/about/passions/page.tsx
@@ -4,48 +4,46 @@ const PassionsPage: NextPage = () => {
 	return (<>
 		<h2>Passions page</h2>
 
-		<div>
-			<p>
-				I am a person with many passions, following the rule "You can't say it's bad until you've tried it."
-				According to this philosophy, I still want and need to give pineapple pizza a chance,
-				despite my Italian blood obliging me to be disgusted by it.
-			</p>
+		<p>
+			I am a person with many passions, following the rule "You can't say it's bad until you've tried it."
+			According to this philosophy, I still want and need to give pineapple pizza a chance,
+			despite my Italian blood obliging me to be disgusted by it.
+		</p>
 
-			<p>
-				I have been practicing Karate since 2010.
-				I grew up in the society I am still part of today: Bushido Karate ASD.
-				For a period, I taught this discipline to children aged 5/6 and up.
-				I am part of the council, taking on various responsibilities, including:
-				<ul>
-					<li>General organization of the society (legislative and bureaucratic)</li>
-					<li>Enrollment management</li>
-					<li>Organization of sporting events</li>
-					<li>Safeguarding policies manager</li>
-					<li>Certified BDSM: first aid intervention</li>
-				</ul>
-			</p>
+		<p>
+			I have been practicing Karate since 2010.
+			I grew up in the society I am still part of today: Bushido Karate ASD.
+			For a period, I taught this discipline to children aged 5/6 and up.
+			I am part of the council, taking on various responsibilities, including:
+			<ul className="list">
+				<li>General organization of the society (legislative and bureaucratic)</li>
+				<li>Enrollment management</li>
+				<li>Organization of sporting events</li>
+				<li>Safeguarding policies manager</li>
+				<li>Certified BDSM: first aid intervention</li>
+			</ul>
+		</p>
 
-			<p>
-				I have been playing the drums since 2021
-				and have been passionate about music even before I was in my parents' thoughts.
-				Always looking for a new concert to attend!
-			</p>
+		<p>
+			I have been playing the drums since 2021
+			and have been passionate about music even before I was in my parents' thoughts.
+			Always looking for a new concert to attend!
+		</p>
 
-			<p>
-				When it comes to the classic "sea or mountains" question, I choose the place with the most beautiful scenery!
-				That it usually coincides with the mountains is just a coincidence...
-				Although I am passionate about hiking and trekking,
-				I currently only have the "Via degli Dei" on the list of "challenging routes completed."
-				But I have many other routes planned!
-				In general, I enjoy traveling, whether in company or solo.
-				I love getting lost in the streets of cities I am exploring for the first time.
-				I have often ended up in extremely peculiar places this way!
-			</p>
+		<p>
+			When it comes to the classic "sea or mountains" question, I choose the place with the most beautiful scenery!
+			That it usually coincides with the mountains is just a coincidence...
+			Although I am passionate about hiking and trekking,
+			I currently only have the "Via degli Dei" on the list of "challenging routes completed."
+			But I have many other routes planned!
+			In general, I enjoy traveling, whether in company or solo.
+			I love getting lost in the streets of cities I am exploring for the first time.
+			I have often ended up in extremely peculiar places this way!
+		</p>
 
-			<p>
-				I have had a B driver's license since 2016 and an A license since 2023.
-			</p>
-		</div>
+		<p>
+			I have had a B driver's license since 2016 and an A license since 2023.
+		</p>
 	</>)
 }
 
--- app/about/studies/page.tsx
+++ app/about/studies/page.tsx
@@ -2,33 +2,30 @@ const StudiesPage = () => {
 	return (<>
 		<h2>Studies page</h2>
 
-		<div>
+		<p>
+			I enrolled at the University of Florence, School of Physics and Astrophysics.
+		</p>
 
-			<p>
-				I enrolled at the University of Florence, School of Physics and Astrophysics.
-			</p>
+		<p>
+			My first approach to computer science was at the university.
+			Among the mandatory first-year courses was an informatics course where they taught C.
+			I immediately liked the subject, so much so that I almost immediately started studying it on my own.
+			On the advice of developer friends, I soon began studying C#.
+		</p>
 
-			<p>
-				My first approach to computer science was at the university.
-				Among the mandatory first-year courses was an informatics course where they taught C.
-				I immediately liked the subject, so much so that I almost immediately started studying it on my own.
-				On the advice of developer friends, I soon began studying C#.
-			</p>
+		<p>
+			I graduated in 2021, defending a thesis on Recommended Systems.
+			A brief description of Recommended Systems: these are models aimed at analyzing a population to predict their behaviors.
+			These systems are ubiquitous today, especially used on e-commerce sites due to their nature.
+			As the name suggests, they aim to recommend specific products to people by analyzing various factors,
+			such as the time of year, age, gender, the culture of the surrounding population,
+			and their behavior within the same e-commerce site.
+			I wrote the various models I studied using Matlab.
+		</p>
 
-			<p>
-				I graduated in 2021, defending a thesis on Recommended Systems.
-				A brief description of Recommended Systems: these are models aimed at analyzing a population to predict their behaviors.
-				These systems are ubiquitous today, especially used on e-commerce sites due to their nature.
-				As the name suggests, they aim to recommend specific products to people by analyzing various factors,
-				such as the time of year, age, gender, the culture of the surrounding population,
-				and their behavior within the same e-commerce site.
-				I wrote the various models I studied using Matlab.
-			</p>
-
-			<p>
-				After graduating, I quickly found a job as a programmer.
-			</p>
-		</div>
+		<p>
+			After graduating, I quickly found a job as a software developer.
+		</p>
 	</>)
 }
 
