b362ff5bebae72306e995489c11bb671e6e40569
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-11 09:23:33 +0200

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import PagesMetaHead from "@/components/layout/PagesMetaHead";
 import { PageContainer } from "../components/page-container";
 import { PageTitle } from "../components/page-title";
+import Image from 'next/image'
 
 export default function Home() {
 	return (<>
-	<PagesMetaHead title="Piero" />
-	
-	<p>Home page</p>
+		<div className="homepage">
+			<div className="homepage-title">
+				<h2>Hi, I'm Federico</h2>
+				<p>And this is my portfolio!</p>
+			</div>
+			
+			<img className="homepage-photo" src="/io.jpg" alt="io" />
+		</div>
 	</>);
 }
