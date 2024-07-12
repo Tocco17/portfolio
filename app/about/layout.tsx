c9a0daa9e188a4df960eea7d4e878cdb6f473dcf
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-12 17:22:26 +0200

    about menu

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { IoIosMenu } from "@react-icons/all-files/io/IoIosMenu";
+import { IoClose } from "@react-icons/all-files/io5/IoClose";
 
 export default function AboutPageLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const [open, setOpen] = useState(false)
+
+	const handleClick = () => {
+		setOpen(prev => !prev)
+	}
 
 	return (<>
 		<div className="menued-paged">
-			<div className="left-menu">
+			<div className={`left-menu ${open ? 'open' : 'closed'}`}>
+				<button className="left-menu-button" onClick={handleClick}>
+					{
+						open 
+						? (<IoClose height={'10em'}/>)
+						: (<IoIosMenu height={'10em'}/>)
+					}
+				</button>
 				<Link className="menu-link" href="/about">Introduction</Link>
 				<Link className="menu-link" href="/about/passions">Passions</Link>
 				<Link className="menu-link" href="/about/studies">Studies</Link>
@@ -21,23 +38,6 @@ export default function AboutPageLayout({
 			<div className="page">
 				{children}
 			</div>
-		</div>
-	</>)
-
-	return (<>
-		<div className="page-with-menu">
-			<div className="horizontal-navbar">
-				<ul>
-					<li><Link href="/about">Introduction</Link></li>
-					<li><Link href="/about/passions">Passions</Link></li>
-					<li><Link href="/about/studies">Studies</Link></li>
-					<li><Link href="/about/work">Work Experience</Link></li>
-					<li><Link href="/about/extra">Extra curriculum experience</Link></li>
-					<li><Link href="/about/skills">Skills</Link></li>
-					<li><Link href="/about/certificates">Certificates</Link></li>
-				</ul>
-			</div>
-			<div className="page">{children}</div>
 		</div>
 	</>)
 }
