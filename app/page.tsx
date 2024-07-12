4b573fb89c63f7c2dee14558c9ae1d39fd8cf825
FedericoRuocco <fede.ruocco@gmail.com>
2024-07-12 13:27:27 +0200

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,41 @@ import Image from 'next/image'
 export default function Home() {
 	return (<>
 		<div className="homepage">
-			<div className="homepage-title">
-				<h2>Hi, I'm Federico</h2>
-				<p>And this is my portfolio!</p>
+			<h1 className="homepage-title">Federico's Portfolio</h1>
+
+			<div className="homepage-page">
+
+				<div className="homepage-paragraph">
+					<h2>Hi, I'm Federico</h2>
+					<p>Wander around to see who I am!</p>
+					<div className="contacts-link">
+
+						<a href="https://www.linkedin.com/in/federico-ruocco/" target="_blank">
+							<img
+								src="linkedin-icon-light.png"
+								alt="Linkedin"
+							/>
+						</a>
+
+						<a href="https://github.com/Tocco17" target="_blank">
+							<img
+								src="github-icon.png"
+								alt="Github"
+							/>
+						</a>
+
+						<a href="mailto:fede.ruocco@gmail.com" target="_blank">
+							<img
+								src="gmail-icon.png"
+								alt="gmail"
+							/>
+						</a>
+
+					</div>
+				</div>
+
+				<img className="homepage-photo" src="/io.jpg" alt="io" />
 			</div>
-			
-			<img className="homepage-photo" src="/io.jpg" alt="io" />
 		</div>
 	</>);
 }
